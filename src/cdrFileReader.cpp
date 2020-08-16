0005280faab83b2ceaa39a966643c3dc37d1ea17
maayan gadasi <maayan1678@gmail.com>
2020-08-16 13:41:29 +0300

    file reader, temporary parser is hard coded

--- src/cdrFileReader.cpp
+++ src/cdrFileReader.cpp
@@ -1,6 +1,7 @@
 #include  "cdrFileReader.hpp"
 #include "fileFormat.hpp"
 #include "exceptions.hpp"
+#include "cdrSender.hpp"
 #include "pipeParser.hpp" // temporary
 using namespace std;
 using namespace files;
@@ -10,19 +11,25 @@ namespace kokfikoCDR {
 
 static const size_t BUFFER_SIZE = 200;
 
+static void ReadFromFile(ifstream& file, IParser* a_parser) {
+    char buffer[BUFFER_SIZE];
+    file.getline(buffer, BUFFER_SIZE); // temporary
+    CdrSender sender;
+    
+    while(file.getline(buffer, BUFFER_SIZE)) {
+        CdrRecord record = a_parser->Parsering(buffer);
+        sender.SendMessage(record);
+    }
+}
+
 void CdrFileReader::ReadFile(const string& a_filename) {
-    ifstream file(a_filename);
+    ifstream file(a_filename.c_str());
     if(file.fail()) {
         throw ExcFileFailedToOpen();
     }
     //IParser* parser = FileFormat::GetParser(file);
     IParser* parser = new PipeParser();
-    char buffer[BUFFER_SIZE];
-
-    file.getline(buffer, BUFFER_SIZE); // temporary
-    while(file.getline(buffer, BUFFER_SIZE)) {
-
-    }
+    ReadFromFile(file, parser);
 }
 
 } // kokfikoCDR
