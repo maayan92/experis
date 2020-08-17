6fff438fea04e065f6b65a6438f5f73528029fdf
maayan gadasi <maayan1678@gmail.com>
2020-08-17 10:32:29 +0300

    add -Wall -Werror to makefiles

--- tests/cdrFileReaderTest/cdrFileReaderTest.cpp
+++ tests/cdrFileReaderTest/cdrFileReaderTest.cpp
@@ -1,7 +1,14 @@
 #include "cdrFileReader.hpp"
-#include "testFunc.hpp"
+#include <iostream>
+using namespace std;
 
 namespace kokfikoCDR {
+
+static void PrintResult(const char* a_test, bool a_result, int& a_testNum, const char* a_tabs) {
+    cout << a_test << ", test number " <<  ++a_testNum << a_tabs
+            << (a_result ? "\033[1;31mSUCCESS" : "\033[1;32mFAILED")
+                << "\033[0m" << endl;
+}
 
 static void TestReadFile() {
     CdrFileReader readFile;
--- tests/tcpClientTest/tcpClientTest.cpp
+++ tests/tcpClientTest/tcpClientTest.cpp
@@ -1,25 +1,65 @@
 #include "tcpClient.hpp"
 #include "exceptions.hpp"
-#include "testFunc.hpp"
+#include <string.h>
 #include <iostream>
 using namespace std;
 using namespace kokfikoCDR;
 using namespace exceptions;
 
-static void TestCreateClient(const TcpClient& a_client) {
+static const size_t BUFFER_SIZE = 200;
+
+static void PrintResult(const char* a_test, bool a_result, int& a_testNum, const char* a_tabs) {
+    cout << a_test << ", test number " <<  ++a_testNum << a_tabs
+            << (a_result ? "\033[1;31mSUCCESS" : "\033[1;32mFAILED")
+                << "\033[0m" << endl;
+}
+
+static void TestConnectToServer(TcpClient& a_client) {
     static int testNum = 0;
-    a_client.SendMessage("message from client");
-    PrintResult("send data", true, testNum, ": \t");
+     try {
+        a_client.ConnectToServer();
+        PrintResult("connection", true, testNum, ": \t");
+
+    }catch(const exception& exc) {
+        cout << exc.what() << endl;
+        PrintResult("connection", false, testNum, ": \t");
+    }
+}
+
+static void TestSendData(TcpClient& a_client) {
+    static int testNum = 0;
+    try {
+        a_client.SendMessage("message from client\n");
+        PrintResult("send data", true, testNum, ": \t");
+
+    }catch(const exception& exc) {
+        cout << exc.what() << endl;
+        PrintResult("send data", false, testNum, ": \t");
+    }
+}
+
+static void TestRecieve(TcpClient& a_client) {
+    static int testNum = 0;
+    try {
+        char buffer[BUFFER_SIZE];
+        a_client.RecvMessage(a_client.GetSocketNumber(), buffer, BUFFER_SIZE);
+        PrintResult("send data", (0 == strcmp(buffer, "message from client\n")), testNum, ": \t");
+
+    }catch(const exception& exc) {
+        cout << exc.what() << endl;
+        PrintResult("send data", false, testNum, ": \t");
+    }
 }
 
 int main() {
     static int testNum = 0;
     try {
         TcpClient client;
-        client.ConnectToServer();
         PrintResult("create client", true, testNum, ": \t");
       
-        TestCreateClient(client);
+        TestConnectToServer(client);
+        TestSendData(client);
+        //TestRecieve(client);
 
     }catch(const exception& exc) {
         cout << exc.what() << endl;
