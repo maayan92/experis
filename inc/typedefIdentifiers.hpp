af497dde9978657051ee4df8a0420a9afcc64fdb
maayan gadasi <maayan1678@gmail.com>
2020-08-20 21:15:51 +0300

    hpp file to save all typedef

---
+++ inc/typedefIdentifiers.hpp
@@ -0,0 +1,22 @@
+#ifndef TYPEDEF_IDENTIFIERS_H
+#define TYPEDEF_IDENTIFIERS_H
+
+#include <iosfwd>
+
+namespace experis {
+
+typedef size_t IMSI;
+typedef size_t MSISDN;
+
+
+typedef size_t OUT_GOING_VC;
+typedef size_t INCOMING_VC;
+typedef size_t INCOMING_VC;
+typedef size_t DATA_TRANSFERRED;
+typedef size_t DATA_RECEIVED;
+typedef size_t SMS_SENT;
+typedef size_t SMS_RECEIVED;
+
+} // experis
+
+#endif
