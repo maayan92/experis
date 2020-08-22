5671974461bac55b5df5ada6998f09c9bbd84a8e
maayan gadasi <maayan1678@gmail.com>
2020-08-22 22:44:25 +0300

    add typedef for customer and for operator

--- inc/typedefIdentifiers.hpp
+++ inc/typedefIdentifiers.hpp
@@ -5,12 +5,13 @@
 
 namespace experis {
 
-typedef size_t IMSI;
-typedef size_t MSISDN;
+typedef size_t IMSI; ////// should be std::string
+typedef size_t MNC; ////// should be std::string
+
+typedef size_t MSISDN; ////// should be std::string
 
 
 typedef size_t OUT_GOING_VC;
-typedef size_t INCOMING_VC;
 typedef size_t INCOMING_VC;
 typedef size_t DATA_TRANSFERRED;
 typedef size_t DATA_RECEIVED;
