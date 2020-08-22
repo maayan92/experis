758bb18d6a045f707c50cd1efedb22e43df2a08c
maayan gadasi <maayan1678@gmail.com>
2020-08-22 22:45:50 +0300

    customer struct to save the customer information

---
+++ inc/customer.hpp
@@ -0,0 +1,53 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include "typedefIdentifiers.hpp"
+#include "cdrRecord.hpp"
+#include <map>
+
+namespace data {
+
+struct SecondParty {
+    size_t m_totalVC;
+    size_t m_totalSms;
+
+    SecondParty& operator+=(const SecondParty& a_secondParty);
+    bool operator==(const SecondParty& a_secondParty) const;
+};
+
+inline SecondParty& SecondParty::operator+=(const SecondParty& a_secondParty) {
+    m_totalVC += a_secondParty.m_totalVC;
+    m_totalSms += a_secondParty.m_totalSms;
+    return *this;
+}
+
+inline bool SecondParty::operator==(const SecondParty& a_secondParty) const {
+    return (m_totalVC == a_secondParty.m_totalVC) &&
+            (m_totalSms == a_secondParty.m_totalSms);
+}
+
+struct Customer {
+    typedef std::map<experis::MSISDN, SecondParty> SecondParties;
+
+    explicit Customer(const kokfikoCDR::CdrRecord& a_record);
+    Customer();
+    //~Customer() = default;
+
+    Customer& operator+=(const Customer& a_customer);
+
+    experis::OUT_GOING_VC m_outgoingVC;
+    experis::INCOMING_VC m_incomingVC;
+    experis::DATA_TRANSFERRED m_totalDataTransf;
+    experis::DATA_RECEIVED m_totalDataReceive;
+    experis::SMS_SENT m_totalSmsSent;
+    experis::SMS_RECEIVED m_totalSmsReceive;
+    SecondParties m_secondParties;
+
+private:
+    void initialization(const kokfikoCDR::CdrRecord& a_record);
+    void setSecondParty(experis::MSISDN a_msisdn, size_t a_totalVC, size_t a_totalSms);
+};
+
+} // data
+
+#endif
---
+++ src/customer.cpp
@@ -0,0 +1,96 @@
+#include "customer.hpp"
+#include <algorithm>
+
+namespace data {
+
+struct CombineSecondParties {
+    CombineSecondParties(Customer::SecondParties& a_secondParties);
+    //~CombineSecondParties() = default;
+
+    void operator()(std::pair<experis::MSISDN, struct SecondParty> a_secondParty);
+
+private:
+    Customer::SecondParties& m_secondParties;
+};
+
+inline CombineSecondParties::CombineSecondParties(Customer::SecondParties& a_secondParties)
+: m_secondParties(a_secondParties)
+{
+}
+
+inline void CombineSecondParties::operator()(std::pair<experis::MSISDN, SecondParty> a_secondParty)
+{
+    m_secondParties[a_secondParty.first] += a_secondParty.second;
+}
+
+Customer::Customer()
+: m_outgoingVC(0)
+, m_incomingVC(0)
+, m_totalDataTransf(0)
+, m_totalDataReceive(0)
+, m_totalSmsSent(0)
+, m_totalSmsReceive(0)
+, m_secondParties()
+{
+}
+
+Customer::Customer(const kokfikoCDR::CdrRecord& a_record) 
+: m_outgoingVC(0)
+, m_incomingVC(0)
+, m_totalDataTransf(0)
+, m_totalDataReceive(0)
+, m_totalSmsSent(0)
+, m_totalSmsReceive(0)
+, m_secondParties()
+{
+    initialization(a_record);
+}
+
+Customer& Customer::operator+=(const Customer& a_customer) {
+    m_outgoingVC += a_customer.m_outgoingVC;
+    m_incomingVC += a_customer.m_incomingVC;
+    m_totalDataTransf += a_customer.m_totalDataTransf;
+    m_totalDataReceive += a_customer.m_totalDataReceive;
+    m_totalSmsSent += a_customer.m_totalSmsSent;
+    m_totalSmsReceive += a_customer.m_totalSmsReceive;
+
+    std::for_each(a_customer.m_secondParties.begin(), a_customer.m_secondParties.end(), CombineSecondParties(m_secondParties));
+    return *this;
+}
+
+// private functions:
+
+void Customer::initialization(const kokfikoCDR::CdrRecord& a_record) {
+    if("MOC" == a_record.m_usageType) {
+        m_outgoingVC = a_record.m_duration;
+        setSecondParty(a_record.m_msisdn, m_outgoingVC, 0);
+    }
+    else if("MTC" == a_record.m_usageType) {
+        m_incomingVC = a_record.m_duration;
+        setSecondParty(a_record.m_msisdn, m_incomingVC, 0);
+    }
+    else if("SMS-MO" == a_record.m_usageType) {
+        m_totalSmsSent = 1;
+        setSecondParty(a_record.m_msisdn, 0, m_totalSmsSent);
+    }
+    else if("SMS-MT" == a_record.m_usageType) {
+        m_totalSmsReceive = 1;
+        setSecondParty(a_record.m_msisdn, 0, m_totalSmsReceive);
+    }
+    else if("D" == a_record.m_usageType) {
+        m_totalDataTransf = a_record.m_byteTransmitted;
+        m_totalDataReceive = a_record.m_byteRecieved;
+    }
+    else {
+        setSecondParty(a_record.m_msisdn, 0, 0);
+    }
+}
+
+void Customer::setSecondParty(experis::MSISDN a_msisdn, size_t a_totalVC, size_t a_totalSms) {
+    SecondParty secondParty;
+    secondParty.m_totalVC = a_totalVC;
+    secondParty.m_totalSms = a_totalSms;
+    m_secondParties.insert(std::make_pair(a_msisdn, secondParty));
+}
+
+} // data
