d7e43374bea961821e7300143917c1d38efd4699
maayan gadasi <maayan1678@gmail.com>
2020-08-25 12:04:28 +0300

    server manager test for getting records from the client

---
+++ tests/serverManager/serverManagerTest.cpp
@@ -0,0 +1,21 @@
+#include "serverManager.hpp"
+#include "mu_test.h"
+#include <climits>
+#include <iostream>
+#include <iomanip>
+
+BEGIN_TEST(create_server_manager)
+    kokfikoCDR::ServerManager serverManager("127.0.0.1", 1234, 5);
+    ASSERT_PASS();
+END_TEST
+
+BEGIN_TEST(server_manager_run)
+    kokfikoCDR::ServerManager serverManager("127.0.0.1", 1234, 5);
+    serverManager.RunServer();
+    ASSERT_PASS();
+END_TEST
+
+BEGIN_SUITE(test)
+    TEST(create_server_manager)
+    TEST(server_manager_run)
+END_SUITE
