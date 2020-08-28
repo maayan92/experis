d2c7503c26be015dfb316cd4c9b4432f38b0dc54
maayan gadasi <maayan1678@gmail.com>
2020-08-28 18:50:51 +0300

    shared pointer tests

---
+++ tests/shared_ptrTest/shared_ptrTest.cpp
@@ -0,0 +1,118 @@
+#include "shared_ptr.hpp"
+#include "mu_test.h"
+#include "testClasses.hpp"
+#include <climits>
+#include<iostream>
+#include<iomanip>
+using namespace advcpp;
+
+BEGIN_TEST(create_shared_ptr)
+    shared_ptr<int> shared_ptr(new int);
+    ASSERT_PASS();
+END_TEST
+
+BEGIN_TEST(create_shared_ptr_inheritance_pow)
+    shared_ptr<Number> shared_ptr(new Pow(8));
+    ASSERT_PASS();
+END_TEST
+
+BEGIN_TEST(create_shared_ptr_inheritance_double)
+    shared_ptr<Number> shared_ptr(new Double(8));
+    ASSERT_PASS();
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_assignment_base)
+    shared_ptr<Number> sharedPtrNum(new Number(5));
+    shared_ptr<Number> sharedPtrPow;
+    sharedPtrPow = sharedPtrNum;
+    ASSERT_THAT(sharedPtrNum == sharedPtrPow);
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_assignment_inheritance)
+    shared_ptr<Number> sharedPtrNum(new Double(5));
+    shared_ptr<Number> sharedPtrPow;
+    sharedPtrPow = sharedPtrNum;
+    ASSERT_THAT(sharedPtrNum == sharedPtrPow);
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_copy_constructor_base)
+    shared_ptr<Number> sharedPtrNum(new Number(5));
+    shared_ptr<Number> sharedPtrPow(sharedPtrNum);
+    ASSERT_THAT(sharedPtrNum == sharedPtrPow);
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_copy_constructor_inheritance)
+    shared_ptr<Number> sharedPtrNum(new Pow(5));
+    shared_ptr<Number> sharedPtrPow(sharedPtrNum);
+    ASSERT_THAT(sharedPtrNum == sharedPtrPow);
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_arrow_derived)
+    shared_ptr<Pow> sharedPtrNum(new Pow(5));
+    ASSERT_EQUAL(25, sharedPtrNum->GetValue());
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_arrow_inheritance)
+    shared_ptr<Number> sharedPtrNum(new Pow(5));
+    ASSERT_EQUAL(25, sharedPtrNum->GetValue());
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_dereference_inheritance)
+    shared_ptr<Number> sharedPtrNum(new Pow(5));
+    ASSERT_EQUAL(25, (*sharedPtrNum).GetValue());
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_dereference_derived)
+    shared_ptr<Pow> sharedPtrNum(new Pow(5));
+    ASSERT_EQUAL(25, (*sharedPtrNum).GetValue());
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_equal_base_true)
+    shared_ptr<Number> sharedPtrNum(new Number(5));
+    shared_ptr<Number> sharedPtrPow(sharedPtrNum);
+    ASSERT_THAT(sharedPtrNum == sharedPtrPow);
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_equal_base_derived_false)
+    shared_ptr<Number> sharedPtrNum(new Number(5));
+    shared_ptr<Number> sharedPtrPow(new Pow(5));
+    ASSERT_THAT(!(sharedPtrNum == sharedPtrPow));
+END_TEST
+
+BEGIN_TEST(shared_ptr_operator_not_equal_base_derived_true)
+    shared_ptr<Number> sharedPtrNum(new Number(5));
+    shared_ptr<Number> sharedPtrPow(new Pow(5));
+    ASSERT_THAT(sharedPtrNum != sharedPtrPow);
+END_TEST
+
+BEGIN_TEST(shared_ptr_reset)
+    shared_ptr<Number> sharedPtrNum(new Number(5));
+    sharedPtrNum.Reset();
+    ASSERT_THAT(!sharedPtrNum);
+END_TEST
+
+BEGIN_SUITE(shared_pointer_tests)
+    TEST(create_shared_ptr)
+    TEST(create_shared_ptr_inheritance_pow)
+    TEST(create_shared_ptr_inheritance_double)
+
+    TEST(shared_ptr_operator_assignment_base)
+    TEST(shared_ptr_operator_assignment_inheritance)
+
+    TEST(shared_ptr_operator_copy_constructor_base)
+    TEST(shared_ptr_operator_copy_constructor_inheritance)
+
+    TEST(shared_ptr_operator_arrow_derived)
+    TEST(shared_ptr_operator_arrow_inheritance)
+
+    TEST(shared_ptr_operator_dereference_derived)
+    TEST(shared_ptr_operator_dereference_inheritance)
+
+    TEST(shared_ptr_operator_equal_base_true)
+    TEST(shared_ptr_operator_equal_base_derived_false)
+
+    TEST(shared_ptr_operator_not_equal_base_derived_true)
+
+    TEST(shared_ptr_reset)
+
+END_SUITE
