1dc1b963ce21bda0e2d8d87588ae190c490ccb56
maayan gadasi <maayan1678@gmail.com>
2020-09-16 15:02:13 +0300

    all the project typedefs

---
+++ smart/inc/typedefs.hpp
@@ -0,0 +1,20 @@
+#ifndef TYPEDEFS_HPP
+#define TYPEDEFS_HPP
+
+#include <string>
+
+namespace experis {
+
+typedef std::string Floor;
+typedef std::string Room;
+typedef std::string EventType;
+
+typedef std::string DeviceId;
+typedef std::string LogFile;
+typedef std::string Configuration;
+
+typedef std::string DeviceType;
+
+} // experis
+
+#endif
