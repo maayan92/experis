9ec7f021c3c6b2fb4abb3bcfe9a175ee08261cc7
maayan gadasi <maayan1678@gmail.com>
2020-09-16 15:02:53 +0300

    struct for saving the location of the device

---
+++ smart/inc/location.hpp
@@ -0,0 +1,17 @@
+#ifndef LOCATION_HPP
+#define LOCATION_HPP
+
+#include "typedefs.hpp"
+
+namespace smart_house {
+
+struct Location {
+
+    experis::Floor m_floor;
+    experis::Room m_room;
+
+};
+
+} // smart_house
+
+#endif
