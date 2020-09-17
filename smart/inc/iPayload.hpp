3a2a296432d9bf93685548885a50bfd4a2ea8594
maayan gadasi <maayan1678@gmail.com>
2020-09-17 08:34:54 +0300

    payload interface, for the data of the event

---
+++ smart/inc/iPayload.hpp
@@ -0,0 +1,13 @@
+#ifndef IPAYLOAD_HPP
+#define IPAYLOAD_HPP
+
+namespace smart_house {
+
+class IPayload {
+public:
+    virtual ~IPayload() = 0;
+};
+
+} // smart_house
+
+#endif
---
+++ smart/src/iPayload.cpp
@@ -0,0 +1,8 @@
+#include "iPayload.hpp"
+
+namespace smart_house {
+
+IPayload::~IPayload()
+{}
+
+} // smart_house
