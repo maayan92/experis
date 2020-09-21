830633ff5ca82c6057915af74b612e7cefed4770
maayan gadasi <maayan1678@gmail.com>
2020-09-21 22:17:14 +0300

    additional functionality for the tests

---
+++ smart/tests/inc/testsFunctions.hpp
@@ -0,0 +1,29 @@
+#ifndef TESTS_FUNCTIONS_HPP
+#define TESTS_FUNCTIONS_HPP
+
+#include "event.hpp"
+#include "controllerTest.hpp"
+#include "payloadSmoke.hpp"
+
+smart_house::Event CreateEvent(smart_house::EventTypeLoc& a_typeLoc)
+{
+    time_t t;
+    time(&t);
+    smart_house::PayloadSmoke ps;
+    smart_house::Event event = {localtime(&t), &ps, a_typeLoc};
+    return event;
+}
+
+template<size_t SIZE>
+bool CheckResult(const smart_house::EventTypeLoc& a_typeLoc, smart_house::ControllerTest* a_controllers)
+{
+    for(size_t i = 0; i < SIZE; ++i) {
+        if(!(a_typeLoc == a_controllers[i].GetEventTypeLocation())) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+#endif
