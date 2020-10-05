501103344b653a49b86c09ef59facca4f4ae806c
maayan gadasi <maayan1678@gmail.com>
2020-10-05 20:04:31 +0300

    add operator== for the tests

--- smart/inc/device.hpp
+++ smart/inc/device.hpp
@@ -13,6 +13,13 @@ struct Device {
     experis::LogFile m_log;
     experis::Configuration m_config;
 
+    bool operator==(const Device& a_device) const {
+        return (m_deviceId == a_device.m_deviceId) &&
+                (m_typeLocation == a_device.m_typeLocation) &&
+                (m_log == a_device.m_log) &&
+                (m_config == a_device.m_config);
+    }
+
 };
 
 } // smart_house
