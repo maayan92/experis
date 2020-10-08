ea2931a57f652b6b2be24f51cc5eb8c4fbd2053f
maayan gadasi <maayan1678@gmail.com>
2020-10-08 08:42:11 +0300

    implementing the template function in a .cpp file by mistake, and fix the iterator definition

--- iq/day2/firstDuplicateT.hpp
+++ iq/day2/firstDuplicateT.hpp
@@ -1,5 +1,6 @@
 #ifndef FIRST_DUPLICATE_T_HPP
 #define FIRST_DUPLICATE_T_HPP
+
 #include <iosfwd>
 #include <tr1/unordered_set>
 
@@ -12,7 +13,36 @@ struct Dummy {
 };
 
 template<typename Container, typename HashFunc = typename Dummy<Container>::Hasher, typename Equal = typename Dummy<Container>::Equal>
-bool FindDuplicateElement(const Container& a_container, typename Container::value_type& a_result);
+bool FindDuplicateElement(const Container& a_container, typename Container::value_type& a_result)
+{
+    typedef std::tr1::unordered_set<typename Container::value_type, HashFunc, Equal> Seen;
+    typedef typename Seen::iterator SeenItr;
+    typedef typename Container::const_reverse_iterator CItr;
+
+    Seen hasSeen;
+    SeenItr seenEnd = hasSeen.end();
+    CItr ritr = a_container.rbegin();
+    CItr rend = a_container.rend();
+
+    CItr where = rend;
+    while(ritr != rend) {
+        SeenItr found = hasSeen.find(*ritr);
+        if(found != seenEnd) {
+            where = ritr;
+        }
+        else {
+            hasSeen.insert(*ritr); 
+        }
+        ++ritr;
+    }
+
+    if(where == rend) {
+        return false;
+    }
+
+    a_result = *where;
+    return true;
+}
 
 } // iq
 
