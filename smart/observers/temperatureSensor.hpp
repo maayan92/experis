8e8fa45555720aab4b7d05333e403d7eb6398322
maayan gadasi <maayan1678@gmail.com>
2021-02-24 20:40:38 +0200

    move all the modules created for the tests from the main project's folders, and set the needed changes

--- smart/observers/temperatureSensor.hpp
+++ smart/observers/temperatureSensor.hpp
@@ -1,18 +1,17 @@
 #ifndef TEMPERATURE_SENSOR_HPP
 #define TEMPERATURE_SENSOR_HPP
 
-#include "iObserver.hpp"
+#include "absSensor.hpp"
 #include "device.hpp"
 
 namespace smart_house {
 
-class TemperatureSensor : public IObserver {
+class TemperatureSensor : public Sensor {
 public:
     TemperatureSensor(const Device& a_deviceDetails);
     TemperatureSensor();
     //virtual ~TemperatureSensor() = default;
 
-    virtual void Notify(const Event& a_newEvent);
     virtual void CreateEvent(const SensorInfo& a_info, struct Event& a_event);
 
 public:
