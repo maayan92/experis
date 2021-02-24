c2278d6a532c6bd6684746094d13845cb40e7762
maayan gadasi <maayan1678@gmail.com>
2021-02-24 19:09:01 +0200

    ler.cpp

---
+++ smart/observers/controller.cpp
@@ -0,0 +1,11 @@
+#include "controller.hpp"
+
+namespace smart_house {
+
+void Controller::CreateEvent(const SensorInfo& a_info, struct Event& a_event)
+{
+    (void)a_info;
+    (void)a_event;
+}
+
+} // smart_house
---
+++ smart/observers/controller.hpp
@@ -0,0 +1,21 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
+
+#include "iObserver.hpp"
+
+namespace smart_house {
+
+class Controller : public IObserver {
+public:
+    virtual ~Controller() = 0;
+    
+    virtual void Notify(const Event& a_newEvent) = 0;
+    virtual void CreateEvent(const SensorInfo& a_info, struct Event& a_event);
+
+protected:
+    Controller();
+};
+
+} // smart_house
+
+#endif
